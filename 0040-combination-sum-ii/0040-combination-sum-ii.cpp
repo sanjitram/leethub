325f5bc1e2c672ca91e273bf2edffc70584dc916
Sanjit Ram <24je0666@iitism.ac.in>
2026-03-13 23:39:57 +0530

    Time: 0 ms (100%), Space: 13.9 MB (98.42%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& v,vector<int>& curr,int i,vector<int>& num,int target){
-        if(target==0){
-            v.push_back(curr);
-            return;
-        }
-        if(i==51 || target<0)    return;
-        if(num[i]>0){
-            curr.push_back(i);
-            num[i]--;
-            dfs(v,curr,i,num,target-i);
-            curr.pop_back();
-            num[i]++;
-            dfs(v,curr,i+1,num,target);
-        }
-        else{
-            dfs(v,curr,i+1,num,target);
-        }
-        
-    }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int> curr;
-        vector<vector<int>> v;
-        int i=0;
-        vector<int> num(51,0);
-        for(int i:candidates)   num[i]++;
-        
-        dfs(v,curr,i,num,target);
-        return v;
+        vector<vector<int>> result;
+        vector<int> tempList;
+        sort(candidates.begin(), candidates.end());
+        backtrack(result, tempList, candidates, target, 0);
+        return result;
+    }
+
+private:
+    void backtrack(vector<vector<int>>& answer, vector<int>& tempList,
+                   vector<int>& candidates, int totalLeft, int index) {
+        if (totalLeft < 0)
+            return;
+        else if (totalLeft == 0) {
+            answer.push_back(tempList);
+        } else {
+            for (int i = index;
+                 i < candidates.size() && totalLeft >= candidates[i]; i++) {
+                if (i > index && candidates[i] == candidates[i - 1]) continue;
+                tempList.push_back(candidates[i]);
+                backtrack(answer, tempList, candidates,
+                          totalLeft - candidates[i], i + 1);
+                tempList.pop_back();
+            }
+        }
     }
 };
