02f34da3f39844bb09066487fcc146b8b15ea088
Sanjit Ram <24je0666@iitism.ac.in>
2026-03-16 17:47:37 +0530

    Time: 9 ms (40.11%), Space: 13.4 MB (30.5%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    int con(string word1,string word2,vector<vector<int>>& dp,int i,int j){
-
-        if(i<0)    return j+1;
-        if(j<0)    return i+1;
-        if(dp[i][j]!=-1)    return dp[i][j];
-        if(word1[i]==word2[j]){
-            return dp[i][j]=con(word1,word2,dp,i-1,j-1);
-        }
-        return dp[i][j]=1+min({con(word1,word2,dp,i-1,j-1),con(word1,word2,dp,i,j-1),con(word1,word2,dp,i-1,j)});
-    }
     int minDistance(string word1, string word2) {
-        if(word1.size()==0 || word2.size()==0)  return max(word1.size(),word2.size());
-        vector<vector<int>> dp(word1.size()+1,vector<int> (word2.size()+1,-1));
-        int i=word1.size()-1,j=word2.size()-1;
-        return con(word1,word2,dp,i,j);
+        vector<vector<int>> dp(word1.size()+1,vector<int> (word2.size()+1,1e5));
+        for(int i=0;i<=word1.size();i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=word2.size();j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=word1.size();i++){
+            for(int j=1;j<=word2.size();j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+min({dp[i][j-1],dp[i-1][j-1],dp[i-1][j]});
+                }
+            }
+        }
+        return dp[word1.size()][word2.size()];
     }
 };
