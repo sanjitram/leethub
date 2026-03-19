30e90c4799fa83fa17ff3c52b9e514a4b61a4fc9
Sanjit Ram <24je0666@iitism.ac.in>
2026-03-19 15:38:06 +0530

    Time: 0 ms (100%), Space: 9.8 MB (79.29%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> p;
+        for(int i:stones)   p.push(i);
+        while(p.size()>1){
+            int a=p.top();
+            p.pop();
+            int b=p.top();
+            p.pop();  
+            int d=a-b;
+            p.push(d);
+        }
+        return p.top();
+    }
+};
