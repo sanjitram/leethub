154e07bfda8c9cac256ae4e3239cc5d2a3ce66a9
Sanjit Ram <24je0666@iitism.ac.in>
2026-03-21 00:52:49 +0530

    Time: 49 ms (5.19%), Space: 28.5 MB (5.12%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    bool found(string &cur, unordered_set<string>& dict){
-        return dict.count(cur);
-    }
-
-    bool dfs(int i, string& s, unordered_set<string>& dict, string cur, vector<vector<int>>& dp){
-        if(i == s.size()){
-            return (cur == "" || found(cur, dict));
+    bool found(string cur,vector<string>& wordDict){
+        for(string s:wordDict){
+            if(cur==s)
+                return true;
         }
-
-        if(dp[i][cur.size()] != -1)
-            return dp[i][cur.size()];
-
-        bool a = false, b = false;
-
-        // extend current substring
-        string next = cur + s[i];
-
-        // option 1: cut if valid
-        if(found(next, dict)){
-            a = dfs(i+1, s, dict, "", dp);
-        }
-
-        // option 2: continue building
-        b = dfs(i+1, s, dict, next, dp);
-
-        return dp[i][cur.size()] = (a || b);
+        return false;
     }
-
+    bool dfs(int i,string& s,vector<string>& wordDict,string& cur,vector<vector<int>>& dp){
+        if(i==s.size()){
+            if(found(cur,wordDict) || cur=="")
+                return true;
+            else
+                return false;
+        }
+        if(dp[i][cur.size()]!=-1){
+            if(dp[i][cur.size()]==1)    return true;
+            return false;
+        }
+        bool a=false,b=false;
+        string t=cur+s[i];
+        string em="";
+        if(found(t,wordDict))
+            a=dfs(i+1,s,wordDict,em,dp);
+        b=dfs(i+1,s,wordDict,t,dp);
+        if(a||b)
+            dp[i][cur.size()]=1;
+        else
+            dp[i][cur.size()]=0;
+        return a||b;
+    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        vector<vector<int>> dp(s.size()+1, vector<int>(s.size()+1, -1));
-        return dfs(0, s, dict, "", dp);
+        string cur="";
+        vector<vector<int>> dp(s.size()+1,vector<int> (s.size()+1,-1));
+        return dfs(0,s,wordDict,cur,dp);
     }
 };
