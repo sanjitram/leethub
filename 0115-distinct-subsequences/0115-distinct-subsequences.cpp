8bdb6a8dfe0b9ca03bf207453b8a518a9dc0c203
Sanjit Ram <24je0666@iitism.ac.in>
2026-03-20 10:40:15 +0530

    Time: 31 ms (53.62%), Space: 44.2 MB (32.99%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int dfs(string& s,string& t,vector<vector<int>>& dp,int i,int j){
-        if(i==0){
-            return 1;
-        }
-        if(j==0){
-            return 0;
-        }
-        if(dp[i][j]!=-1)    return dp[i][j];
-        if(t[i-1]==s[j-1])
-            return dp[i][j]=dfs(s,t,dp,i,j-1)+dfs(s,t,dp,i-1,j-1);
-        return dp[i][j]=dfs(s,t,dp,i,j-1);
-    }
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(t.size()+1,vector<int> (s.size()+1,-1));
-        return dfs(s,t,dp,t.size(),s.size());
+        vector<vector<unsigned long long>> dp(t.size()+1,vector<unsigned long long> (s.size()+1,0));
+        for(int i=0;i<=t.size();i++)
+            dp[i][0]=0;
+        for(int j=0;j<=s.size();j++) 
+            dp[0][j]=1;
+        for(int i=1;i<=t.size();i++){
+            for(int j=1;j<=s.size();j++){
+                if(t[i-1]==s[j-1])
+                    dp[i][j]=dp[i][j-1]+dp[i-1][j-1];
+                else
+                    dp[i][j]=dp[i][j-1];
+            }
+        }
+        return dp[t.size()][s.size()];
     }
 };
